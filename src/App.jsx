538c952bdd262760ae9c0675da5a2b5a4ddf1b78
Jaydeepsinh Parmar <148114163+jaydeep099@users.noreply.github.com>
2024-11-04 18:18:24 +0530

    Hero page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css';
-import Contact from './sections/Contact/Contact';
+// import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
-import Skills from './sections/Skills/Skills';
+// import Projects from './sections/Projects/Projects';
+// import Skills from './sections/Skills/Skills';
 
 function App() {
   return (
     <>
       <Hero />
-      <Projects />
-      <Skills />
-      <Contact />
+      {/* <Projects /> */}
+      {/* <Skills /> */}
+      {/* <Contact /> */}
       <Footer />
     </>
   );
